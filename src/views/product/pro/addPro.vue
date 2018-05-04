8e6d32da62e6b214a62255bed99a8e6e059b0f3d
master-joy <master-joy@mac.local>
2018-05-04 22:23:24 +0800

    继续添加

--- src/views/product/pro/addPro.vue
+++ src/views/product/pro/addPro.vue
@@ -39,6 +39,7 @@
                 <el-col :span="8">
                     <el-form-item label="售价">
                         <el-input style="width: 200px"
+                            type="number"
                             v-model.number="formValue.price"></el-input>
                     </el-form-item>
                 </el-col>
@@ -47,18 +48,21 @@
                 <el-col :span="8">
                     <el-form-item label="库存">
                         <el-input style="width: 200px"
+                            type="number"
                             v-model.number="formValue.stock"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="8">
                     <el-form-item label="售出">
                         <el-input style="width: 200px"
+                            type="number"
                             v-model.number="formValue.sales_volume"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="8">
                     <el-form-item label="排序">
                         <el-input style="width: 200px"
+                            type="number"
                             v-model.number="formValue.sort"></el-input>
                     </el-form-item>
                 </el-col>
@@ -132,7 +136,6 @@
                         highlight-current-row
                         class="table-sku">
                         <el-table-column prop="name"
-                            width="20%"
                             label="属性名">
                             <template slot-scope="scope">
                                 <el-input v-model="scope.row.name"
@@ -140,7 +143,6 @@
                             </template>
                         </el-table-column>
                         <el-table-column prop="market_price"
-                            width="15%"
                             label="市场价">
                             <template slot-scope="scope">
                                 <el-input v-model.number="scope.row.market_price"
@@ -149,7 +151,6 @@
                             </template>
                         </el-table-column>
                         <el-table-column prop="price"
-                            width="15%"
                             label="售价">
                             <template slot-scope="scope">
                                 <el-input v-model.number="scope.row.price"
@@ -158,7 +159,6 @@
                             </template>
                         </el-table-column>
                         <el-table-column prop="stock"
-                            width="15%"
                             label="库存">
                             <template slot-scope="scope">
                                 <el-input v-model.number="scope.row.stock"
@@ -167,12 +167,31 @@
                             </template>
                         </el-table-column>
                         <el-table-column prop="sales_volume"
-                            width="15%"
                             label="销量">
                             <template slot-scope="scope">
                                 <el-input v-model.number="scope.row.sales_volume"
                                     type="number"
                                     size="mini"></el-input>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="img"
+                            align="center"
+                            label="上传图片">
+                            <template slot-scope="scope">
+                                <el-upload :on-success="skuImgUploadSuccess"
+                                    :on-progress="skuImgUpload(scope.$index)"
+                                    :show-file-list="false"
+                                    :action=imgPostUrl>
+                                    <el-button slot="trigger"
+                                        v-if="!scope.row.img"
+                                        size="small"
+                                        type="primary">选取文件</el-button>
+                                    <img :src="scope.row.img"
+                                        v-else
+                                        style="height: 30px; width: 30px;"
+                                        slot="trigger"
+                                        alt="">
+                                </el-upload>
                             </template>
                         </el-table-column>
                         <el-table-column prop="sales_volume"
@@ -224,7 +243,7 @@ const api = process.env.BASE_API
 const cdn = process.env.CDN
 export default {
     name: "addPro",
-    data() {
+    data () {
         return {
             formValue: {
                 title: "",
@@ -252,18 +271,19 @@ export default {
             sku2List: [],
             chooseSku1: [],
             chooseSku2: [],
+            tempIdenx: 0, //临时保存SKU上传图片的变量
             imgPostUrl: api + '/common/uploadImg', //图片上传地址
             dialogImageUrl: '', //已完成上传, 加载图片预览地址
             dialogVisible: false, //加载图片预览地址flag
         }
     },
-    created() {
+    created () {
         getCate().then(res => {
             this.cate = res.data
         })
     },
     methods: {
-        chooseCate(tag) {
+        chooseCate (tag) {
             this.skuLabel = []
             this.chooseSku1 = []
             this.chooseSku2 = []
@@ -276,7 +296,7 @@ export default {
                 }
             })
         },
-        setSku(sku) {
+        setSku (sku) {
             const sku1 = []
             const sku2 = []
             sku.forEach((item, index) => {
@@ -290,62 +310,73 @@ export default {
             this.sku2List = sku2.length > 0 ? sku2 : []
         },
         //图片预览
-        handlePictureCardPreview(file) {
+        handlePictureCardPreview (file) {
             console.log('handlePictureCardPreview', file)
             this.dialogVisible = true
             this.dialogImageUrl = cdn + file.response.data
         },
         //图片删除钩子
-        handleRemove(file, fileList) {
+        handleRemove (file, fileList) {
             this.formValue.imgs.splice(inArray(this.formValue.imgs, file.response.data), 1)
             console.log('handleRemove', file, fileList)
         },
         //图片上传钩子
-        uploadSuccess(res, file, fileList) {
+        uploadSuccess (res, file, fileList) {
             this.formValue.imgs.unshift(res.data)
             console.log('uploadSuccess', res, file, fileList)
         },
         //图片删除前钩子
-        beforeRemove(file, fileList) {
+        beforeRemove (file, fileList) {
             console.log('beforeRemove', file, fileList)
             return this.$confirm('确定移除？')
         },
         //删除SKU
-        deleteSku(index, row) {
+        deleteSku (index, row) {
             this.formValue.sku.splice(index, 1)
         },
+        //SKU图片上传之前的操作
+        skuImgUpload (index) {
+            this.tempIdenx = index
+        },
+        //SKU图片上传
+        skuImgUploadSuccess (res) {
+            if (res.status == 1) {
+                this.formValue.sku[this.tempIdenx].img = cdn + res.data
+                console.log(this.formValue.sku[this.tempIdenx])
+            }
+        },
         //提交
-        onSubmit() {
+        onSubmit () {
             addPro(this.formValue).then(res => {
                 console.log(res)
             })
         },
         // 添加新的SKU属性, 笛卡尔积
-        buildChild() {
+        buildChild () {
             // console.log(this.chooseSku1, this.chooseSku2)
             this.formValue.sku = []
             if (this.chooseSku1.length > 0 && this.chooseSku2.length == 0) {
                 this.chooseSku1.forEach((item, index) => {
-                    this.formValue.sku.push({ name: item.name, sku_id_1: item.id, sku_id_2: 0, stock: 0, market_price: 0, price: 0, sales_volume: 0 })
+                    this.formValue.sku.push({ name: item.name, sku_id_1: item.id, sku_id_2: 0, stock: 0, market_price: 0, price: 0, sales_volume: 0, img: '' })
                 })
             } else if (this.chooseSku1.length > 0 && this.chooseSku2.length > 0) {
                 this.chooseSku1.forEach((item, index) => {
                     this.chooseSku2.forEach((item2, index2) => {
-                        this.formValue.sku.push({ name: item.name + item2.name, sku_id_1: item.id, sku_id_2: item2.id, stock: 0, market_price: 0, price: 0, sales_volume: 0 })
+                        this.formValue.sku.push({ name: item.name + item2.name, sku_id_1: item.id, sku_id_2: item2.id, stock: 0, market_price: 0, price: 0, sales_volume: 0, img: '' })
                     })
                 })
             } else if (this.chooseSku1.length == 0 && this.chooseSku2.length > 0) {
                 this.chooseSku2.forEach((item, index) => {
-                    this.formValue.sku.push({ name: item.name, sku_id_1: 0, sku_id_2: item.id, stock: 0, market_price: 0, price: 0, sales_volume: 0 })
+                    this.formValue.sku.push({ name: item.name, sku_id_1: 0, sku_id_2: item.id, stock: 0, market_price: 0, price: 0, sales_volume: 0, img: '' })
                 })
             }
         }
     },
     watch: {
-        chooseSku1(newV, oldV) {
+        chooseSku1 (newV, oldV) {
             this.buildChild()
         },
-        chooseSku2(newV, oldV) {
+        chooseSku2 (newV, oldV) {
             this.buildChild()
         },
     },
